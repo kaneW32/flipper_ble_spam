71ac4f5c91ccee26714602c3e3e4c0a4531f42f7
kaneW32 <kanewitso1@gmail.com>
2025-02-28 20:32:30 -0800

    Update ble_spam.c

--- applications_user/ble_spam /ble_spam.c
+++ applications_user/ble_spam /ble_spam.c
@@ -1,39 +1,29 @@
 #include <furi.h>
 #include <gui/gui.h>
 #include <input/input.h>
-#include <furi_hal_bt.h>
-#include <furi_thread.h>
+#include <furi_hal_bt.h>  // For furi_hal_bt_send_adv
 
 #define DEFAULT_SPAM_RATE 500
 
-static uint32_t spam_rate = DEFAULT_SPAM_RATE;
 static bool running = true;
-
-void ble_spam_task(void* p) {
-    UNUSED(p);
-    while (running) {
-        furi_hal_bt_send_adv("Flipper_BLE_Spam", 16, true);
-        furi_delay_ms(spam_rate);
-    }
-}
-
-void input_callback(InputEvent* event, void* context) {
-    UNUSED(context);
-}
 
 int32_t ble_spam_app(void* p) {
     UNUSED(p);
+
+    // Open necessary Flipper records
     Gui* gui = furi_record_open(RECORD_GUI);
     FuriInput* input = furi_record_open(RECORD_INPUT);
-    input_set_callback(input, input_callback, NULL);
-    
-    running = true;
-    furi_thread_create(ble_spam_task, NULL, 1024);
-    
-    while (running) {
-        furi_delay_ms(100);
+    // (Optional) set an input callback if you want to handle button presses
+    // input_set_callback(input, your_input_callback, NULL);
+
+    // Main loop
+    while(running) {
+        furi_hal_bt_send_adv("Flipper_BLE_Spam", 16, true);
+        furi_delay_ms(DEFAULT_SPAM_RATE);
+        // Possibly check for a condition to break out of the loop
     }
-    
+
+    // Cleanup
     furi_record_close(RECORD_INPUT);
     furi_record_close(RECORD_GUI);
     return 0;
